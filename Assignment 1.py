a199f7e9de693cc57daaa1684de56d5a1666d453
vedantturkar8-afk <vedantturkar8@gmail.com>
2026-01-05 13:12:25 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,13 @@
+# ASSIGNMENT 1
+
+a= float(input("Enter the first number:"))
+b= float(input("Enter the second number:"))
+
+c = a + b
+print("addition:",c)
+d = a - b
+print("subtraction:",d)
+e = a * b
+print("multiplication:",e)
+f = a / b
+print("division:",f)
